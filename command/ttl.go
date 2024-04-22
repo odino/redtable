60a4f9813a70b7f8bda187f56cdfbed6237cf766
odino <alessandro.nadalin@gmail.com>
2024-04-22 20:54:38 +0400

    `TTL`

--- command/ttl.go
+++ command/ttl.go
@@ -26,23 +26,17 @@ func (cmd *TTL) Parse(args []resp.Arg) error {
 func (cmd *TTL) Run(ctx context.Context, tbl *bigtable.Table) (any, error) {
 	row, ok, err := util.GetRow(ctx, cmd.Key, tbl)
 
-	val := -1
-
 	if err != nil {
 		return nil, err
 	}
 
 	if !ok {
-		val = -2
-	}
-
-	if row.Expiry.IsZero() {
-		val = -1
+		return resp.SimpleInt(-2), nil
 	}
 
 	if time.Until(row.Expiry) >= 0 {
-		val = int(time.Until(row.Expiry).Round(time.Second).Seconds())
+		return resp.SimpleInt(int(time.Until(row.Expiry).Round(time.Second).Seconds())), nil
 	}
 
-	return resp.SimpleInt(val), nil
+	return resp.SimpleInt(-1), nil
 }
